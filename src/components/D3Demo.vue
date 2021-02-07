1d16a06ce3126bcbf00c32f25f75f5a8f9d616d4
Carryours <36062794+Carryours@users.noreply.github.com>
2021-02-07 21:32:38 +0800

    init g3

---
+++ src/components/D3Demo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>111</div>
+</template>
+
+<script>
+import * as d3 from "d3";
+export default {
+  mounted() {
+    const data = d3
+      .csvParse(
+        FileAttachment("us-population-state-age.csv").text(),
+        (d, i, columns) => (
+          d3.autoType(d), (d.total = d3.sum(columns, (c) => d[c])), d
+        )
+      )
+      .sort((a, b) => b["<10"] / b.total - a["<10"] / a.total);
+    console.log(data);
+  },
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './components/D3Demo.vue'
 
 Vue.config.productionTip = false
 
