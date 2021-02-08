756b808a3f63fbd3861ebcf7a06f5cdafb0a257a
Carryours <36062794+Carryours@users.noreply.github.com>
2021-02-09 00:08:03 +0800

    add demo2

--- src/components/D3Demo2.vue
+++ src/components/D3Demo2.vue
@@ -14,7 +14,7 @@ export default {
   },
   methods: {
     draw() {
-      const margin = { top: 30, right: 10, bottom: 0, left: 30 };
+      const margin = { top: 10, right: 10, bottom: 20, left: 40 };
       axios
         .request({
           baseURL: "http://127.0.0.1:2021",
@@ -31,8 +31,9 @@ export default {
             )
             .sort((a, b) => b["<10"] / b.total - a["<10"] / a.total);
           console.log(data);
-          const width = 1200;
-          const height = data.length * 25 + margin.top + margin.bottom;
+          const width = 1000;
+          // const height = data.length * 25 + margin.top + margin.bottom;
+          const height = 600;
           const series = d3
             .stack()
             .keys(data.columns.slice(1))
@@ -40,14 +41,18 @@ export default {
             .map((d) => (d.forEach((v) => (v.key = d.key)), d));
 
           console.log(series);
-
-          const x = d3.scaleLinear().range([margin.left, width - margin.right]);
+          // let temp = 0
+          const x = d3
+            .scaleBand()
+            // .domain(data.map(() => temp+=1))
+            .domain(data.map((d) => d.name))
+            .range([margin.left, width - margin.right])
+            .padding(0.1);
 
           const y = d3
-            .scaleBand()
-            .domain(data.map((d) => d.name))
-            .range([margin.top, height - margin.bottom])
-            .padding(0.08);
+            .scaleLinear()
+            .domain([0, d3.max(series, (d) => d3.max(d, (d) => d[1]))])
+            .rangeRound([height - margin.bottom, margin.top]);
 
           const color = d3
             .scaleOrdinal()
@@ -60,23 +65,35 @@ export default {
           const formatValue = (x) =>
             isNaN(x) ? "N/A" : x.toLocaleString("en");
 
+          const tick = [];
+          for (let i = 100; i < 5200; i += 100) {
+            tick.push(i);
+          }
+          const xAxis = (g) =>
+            g
+              .attr("transform", `translate(0,${height - margin.bottom})`)
+              .call(d3.axisBottom(x).tickSizeOuter(100))
+              .call((g) => g.selectAll(".domain").remove());
+
           d3.select(".demo")
             .append("svg")
             .attr("viewBox", [0, 0, width, height])
-            .style("overflow", "visible")
+            // .append("g").call(xAxis);
+            // .style("overflow", "visible")
+            // .append("g")
+            // .call(xAxis)
             .append("g")
             .selectAll("g")
             .data(series)
-            .enter()
-            .append("g")
+            .join("g")
             .attr("fill", (d) => color(d.key))
             .selectAll("rect")
             .data((d) => d)
             .join("rect")
-            .attr("x", (d) => x(d[0]))
-            .attr("y", (d) => y(d.data.name))
-            .attr("width", (d) => x(d[1]) - x(d[0]))
-            .attr("height", y.bandwidth())
+            .attr("x", (d) => x(d.data.name))
+            .attr("y", (d) => y(d[1]))
+            .attr("height", (d) => y(d[0]) - y(d[1]))
+            .attr("width", x.bandwidth())
             .append("title")
             .text(
               (d) =>
@@ -84,6 +101,17 @@ export default {
                   d[1] - d[0]
                 )} (${formatValue(d.data[d.key])})`
             );
+          console.log(
+            d3
+              .select(".demo")
+              .select("svg")
+              .select("g")
+          );
+          d3.select(".demo")
+            .select("svg")
+            .select("g")
+            .append("g")
+            .call(xAxis);
         });
     },
     renderChart() {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './components/D3Demo.vue'
+import App from './components/D3Demo2.vue'
 
 Vue.config.productionTip = false
 
